1d087ff119471fdb9fe2439fe5a384742669da00
anav5704 <anavkki@gmail.com>
2024-01-15 14:26:14 +1200

    feat: add about route

---
+++ src/routes/about/index.tsx
@@ -0,0 +1,17 @@
+import { component$ } from "@builder.io/qwik"
+
+export default component$(() => {
+    return (
+        <main>
+            <h1>About Page</h1>
+            <article>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Pariatur tempore mollitia optio voluptates veniam iusto sit architecto eos! Veritatis, vitae ut? Quos odit eaque eum consequatur molestiae aut ab laudantium quaerat vitae facere? Ipsum incidunt dolor sapiente voluptatum quas iste asperiores dolorum ea, nisi quasi praesentium harum vero, reprehenderit alias?</p>
+                <br />
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Pariatur tempore mollitia optio voluptates veniam iusto sit architecto eos! Veritatis, vitae ut? Quos odit eaque eum consequatur molestiae aut ab laudantium quaerat vitae facere? Ipsum incidunt dolor sapiente voluptatum quas iste asperiores dolorum ea, nisi quasi praesentium harum vero, reprehenderit alias?</p>
+                <br />
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Pariatur tempore mollitia optio voluptates veniam iusto sit architecto eos! Veritatis, vitae ut? Quos odit eaque eum consequatur molestiae aut ab laudantium quaerat vitae facere? Ipsum incidunt dolor sapiente voluptatum quas iste asperiores dolorum ea, nisi quasi praesentium harum vero, reprehenderit alias?</p>
+                <br />
+            </article>
+        </main>
+    )
+})
