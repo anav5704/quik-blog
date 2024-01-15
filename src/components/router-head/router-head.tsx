7d9a4305c29f78a1afcbc64b40e7136fa96d5963
anav5704 <anavkki@gmail.com>
2024-01-15 15:05:26 +1200

    feat: add links and layout

--- src/components/router-head/router-head.tsx
+++ src/components/router-head/router-head.tsx
@@ -6,32 +6,35 @@ import { component$ } from "@builder.io/qwik";
  * The RouterHead component is placed inside of the document `<head>` element.
  */
 export const RouterHead = component$(() => {
-  const head = useDocumentHead();
-  const loc = useLocation();
+    const head = useDocumentHead();
+    const loc = useLocation();
 
-  return (
-    <>
-      <title>{head.title}</title>
+    return (
+        <>
+            <title>{head.title}</title>
 
-      <link rel="canonical" href={loc.url.href} />
-      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+            <link rel="canonical" href={loc.url.href} />
+            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+            <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+            <link rel="preconnect" href="https://fonts.googleapis.com" />
+            <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+            <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" />
 
-      {head.meta.map((m) => (
-        <meta key={m.key} {...m} />
-      ))}
+            {head.meta.map((m) => (
+                <meta key={m.key} {...m} />
+            ))}
 
-      {head.links.map((l) => (
-        <link key={l.key} {...l} />
-      ))}
+            {head.links.map((l) => (
+                <link key={l.key} {...l} />
+            ))}
 
-      {head.styles.map((s) => (
-        <style key={s.key} {...s.props} dangerouslySetInnerHTML={s.style} />
-      ))}
+            {head.styles.map((s) => (
+                <style key={s.key} {...s.props} dangerouslySetInnerHTML={s.style} />
+            ))}
 
-      {head.scripts.map((s) => (
-        <script key={s.key} {...s.props} dangerouslySetInnerHTML={s.script} />
-      ))}
-    </>
-  );
+            {head.scripts.map((s) => (
+                <script key={s.key} {...s.props} dangerouslySetInnerHTML={s.script} />
+            ))}
+        </>
+    );
 });
