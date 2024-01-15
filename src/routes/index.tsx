92de38e94c079e34c178e8ecb63fadab2a4339d6
anav5704 <anavkki@gmail.com>
2024-01-15 15:38:36 +1200

    feat: add useSignal and useStore

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,4 +1,4 @@
-import { component$ } from "@builder.io/qwik";
+import { component$, useSignal, useStore } from "@builder.io/qwik";
 import type { DocumentHead } from "@builder.io/qwik-city";
 
 import Counter from "../components/starter/counter/counter";
@@ -7,10 +7,27 @@ import Infobox from "../components/starter/infobox/infobox";
 import Starter from "../components/starter/next-steps/next-steps";
 
 export default component$(() => {
-    return (
-        <main>
+    const name = useSignal("")
+    const blogs = useStore([
+        {
+            name: "Blog 1",
+            author: "Anav"
+        },
+        {
+            name: "Blog 2",
+            author: "Anav"
+        }
+    ])
 
-        </main>
+    return (
+        <div>
+            {name.value}
+            <button onClick$={() => name.value = "Anav"} >First Name</button>
+            <button onClick$={() => name.value = "Chand"}>Surname</button>
+            <article>
+                {blogs.map(blog => blog.name)}
+            </article>
+        </div>
     );
 });
 
